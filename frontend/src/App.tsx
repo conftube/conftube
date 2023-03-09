539d4740a64d6c742b6120ad752447851e6283ec
Johanna Liebler <liebler@e2n.de>
2023-03-09 20:44:16 +0100

    initial docker and add apollo and add favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,60 @@
 import "./App.css";
-import Search from "./components/Search";
-import Content from "./components/Content";
+import { useQuery, gql } from "@apollo/client";
 
-function App() {
+export default function App() {
+  const GET_SEARCHVIDEO = gql`
+    query GetSearchVideo {
+      searchVideos("JavaScript") {
+        title
+        description
+        platform
+        thumbnailUrl
+        id
+        publishedAt
+      }
+    }
+  `;
+
+  function DisplayVideo() {
+    const { loading, error, data } = useQuery(GET_SEARCHVIDEO);
+    console.log(data);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+
+    return data.map(
+      ({
+        title,
+        description,
+        platform,
+        thumbnailUrl,
+        id,
+        publishedAt,
+      }: {
+        title: string;
+        description: string;
+        platform: string;
+        thumbnailUrl: string;
+        id: number;
+        publishedAt: string;
+      }) => (
+        <div key={id}>
+          <h3>{title}</h3>
+          <video width="400" height="250" src={`${thumbnailUrl}`} />
+          <br />
+          <p>{description}</p>
+          <p>{platform}</p>
+          <p>{publishedAt}</p>
+          <br />
+        </div>
+      )
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Search />
-        <Content />
-      </header>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <DisplayVideo />
     </div>
   );
 }
-
-export default App;
